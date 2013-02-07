36ec29a90d40d87f6a4814e9a9d447adbbe53ea3
Phil Ostler <philostler@gmail.com>
2013-02-07 21:48:13 +0000

    Rolled Crossroads.js up to 0.12.0

--- spec/crossroadsjs/rails/version_spec.rb
+++ spec/crossroadsjs/rails/version_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
 describe Crossroadsjs::Rails do
-  it { Crossroadsjs::Rails::VERSION.should == "1.4.0" }
-  it { Crossroadsjs::Rails::CROSSROADSJS_VERSION.should == "0.11.0" }
+  it { Crossroadsjs::Rails::VERSION.should == "1.5.0" }
+  it { Crossroadsjs::Rails::CROSSROADSJS_VERSION.should == "0.12.0" }
 end
